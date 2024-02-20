9caf43583c55e556a3cade38c3fed1eb806e0578
JuanSoM <0610948742@uma.es>
2024-02-20 18:58:48 +0100

    implementacion recursiva

--- factorial.java
+++ factorial.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class factorial {
 
     public static long fact(long num) {
-        long fac = 1;
-        for (long i = 1; i <= num; ++i) {
-            fac = fac * i;
+        if (num<=0) {
+        return 1;
+        } else {
+        return num*fact(num-1);
         }
-        return fac;
     }
 
     public static void main(String[] args) {
@@ -18,4 +18,5 @@ public class factorial {
         System.out.println("El factorial de " + num + " es " + fac);
         scanInput.close();
     }
+
 }
